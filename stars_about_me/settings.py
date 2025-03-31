5529521adf38c953455b514dffd4400d986e312b
andongoshev <andon.go6ev@gmail.com>
2025-03-31 10:07:09 +0300

    deploy - fixed .env settings

--- stars_about_me/settings.py
+++ stars_about_me/settings.py
@@ -8,12 +8,12 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!g(#0%%6z7w%w!o8e(907&muy6d2i31)_ciw@k&4&sp)edi7)j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.0.156', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
