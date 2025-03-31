5efc1e50ee6abb260cf25cb49d3af0f00260a54a
andongoshev <andon.go6ev@gmail.com>
2025-03-31 10:18:14 +0300

    fixed allowed hosts

--- stars_about_me/settings.py
+++ stars_about_me/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', )
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
