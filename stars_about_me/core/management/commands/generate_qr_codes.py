6d549bd08539c07f5544201cba528d8c99a2f144
andongoshev <andon.go6ev@gmail.com>
2025-04-06 20:13:35 +0300

    added loading animation ,added scrolling when sign is selected

--- stars_about_me/core/management/commands/generate_qr_codes.py
+++ stars_about_me/core/management/commands/generate_qr_codes.py
@@ -7,7 +7,7 @@ from django.core.management import BaseCommand
 from stars_about_me import settings
 from stars_about_me.core.models import QrCode
 
-QR_FOLDER = 'media/testcodes/'
+QR_FOLDER = 'media/production_codes/'
 
 class Command(BaseCommand):
     help = 'Generate QR codes'
@@ -23,9 +23,23 @@ class Command(BaseCommand):
 
         for _ in range(count):
             qr_id = str(uuid.uuid4())[:8]
-            qr_url = f'localhost:8000/luckies/{qr_id}.jpg'
+            qr_url = f'starsaboutme.com/luckies/{qr_id}'
 
             qr = qrcode.make(qr_url)
+            qr = qr.convert("RGBA")  # Convert to RGBA to allow transparency
+
+            # Make white pixels transparent
+            datas = qr.getdata()
+            new_data = []
+            for item in datas:
+                # Change white (255, 255, 255) to transparent (255, 255, 255, 0)
+                if item[:3] == (255, 255, 255):
+                    new_data.append((255, 255, 255, 0))
+                else:
+                    new_data.append(item)
+
+            qr.putdata(new_data)
+
             qr_path = os.path.join(QR_FOLDER, f"{qr_id}.png")
             qr.save(qr_path)
 
---
+++ static/js/loader.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired");
+
+    // Wait for the window to load completely
+    window.addEventListener("load", function () {
+        const loader = document.getElementById("loader");
+
+        if (loader) {
+            console.log("Loader is visible now.");
+            loader.style.visibility = "visible";  // Make loader visible
+            loader.style.opacity = 1;  // Fade in the loader
+
+            // Wait for a specified time before hiding the loader
+            setTimeout(function () {
+                loader.style.opacity = 0;  // Fade out the loader
+                loader.style.visibility = "hidden";  // Hide it completely after fade
+            }, 1000); // Adjust the delay as per your requirement (e.g., 2 seconds)
+        } else {
+            console.log("Loader element not found!");
+        }
+    });
+});
